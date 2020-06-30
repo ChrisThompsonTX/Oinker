991fcfcb08f0755f64894f39b22efd4130608a2f
Chris Thompson <chris.tx86@gmail.com>
2020-06-30 13:39:21 -0700

    initialize back end get response

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-
+app.get('/', (req,res)=> {
+    res.json({
+        message: "hello"
+    });
+});
 
 app.listen(5000, ()=> {
-
-})
+    console.log('Listening on http://localhost:5000')
+});
